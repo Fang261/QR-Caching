ec0ed5c78ed4c9b51116685b9eabf037456e0389
PrimedFortuna <primed.5353@gmail.com>
2023-11-10 15:17:25 +0000

    fuixed

--- app/src/main/java/pt/iade/joaotomas/QRCaching/map.java
+++ app/src/main/java/pt/iade/joaotomas/QRCaching/map.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import pt.iade.joaomoniz.copypasteplayground.R;
+
 public class map extends AppCompatActivity {
 
     @Override
