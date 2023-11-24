6952d2cdeb5c37a5e03695904eaf3c427061627c
Fang261 <tomasfss04@gmail.com>
2023-11-24 13:30:18 +0000

    mudar butoes e acrescentar activity eventlist

---
+++ app/src/main/java/pt/iade/joaotomas/QRCaching/eventlist.java
@@ -0,0 +1,16 @@
+package pt.iade.joaotomas.QRCaching;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import pt.iade.joaomoniz.copypasteplayground.R;
+
+public class eventlist extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_eventlist);
+    }
+}
