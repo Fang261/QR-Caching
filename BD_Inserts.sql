a8c0331909dd8882a85c7cb7e3d96cc40b13be97
Fang261 <tomasfss04@gmail.com>
2023-11-27 00:01:49 +0000

    Final 2 entrega

---
+++ BD_Inserts.sql
@@ -0,0 +1,122 @@
+insert into user (user_name, user_phone, user_bdate, user_password, user_email) values ('Tiago Estrela', 961789645, null,'password123.','tiago.estrela@gmail.com');  
+insert into user (user_name, user_phone, user_bdate, user_password, user_email) values ('Diogo Menezes', 987345263, null,'gaspar345.','diogo.goldgold@gmail.com');  
+insert into user (user_name, user_phone, user_bdate, user_password, user_email) values ('Diogo Couto', 912346543, null,'planepiupiu04.','diogo.war@sapo.pt');  
+insert into user (user_name, user_phone, user_bdate, user_password, user_email) values ('João Figueiras', 937373483, null,'henrique999.','sou.winner@gmail.com');  
+insert into user (user_name, user_phone, user_bdate, user_password, user_email) values ('André Guimarães', 975483375, null,'soulindoo1.','aventura.99@sapo.pt');  
+insert into user (user_name, user_phone, user_bdate, user_password, user_email) values ('António Costa', 902846289, null,'geringonça22.','gambalamba123@gmail.com');  
+insert into user (user_name, user_phone, user_bdate, user_password, user_email) values ('Gaspar Madeira', 987364738, null,'vodka.333','adoro.te.mae@sapo.pt');
+insert into user (user_name, user_phone, user_bdate, user_password, user_email) values ('Duarte Alves',	932467728, null, 'jocajoca224.', 'duarte.alves@gmail.com');
+insert into user (user_name, user_phone, user_bdate, user_password, user_email) values ('Leandro Silva', 912336782, null, 'beerbike.23', 'leo.silva22@sapo.pt');
+insert into user (user_name, user_phone, user_bdate, user_password, user_email) values ('Joana Sousa', 932667283, null, 'twt33do', 'sousa.jj33@gmail.com');
+insert into user (user_name, user_phone, user_bdate, user_password, user_email) values ('Filipe Santos', 961908572, null, 'zambu4Life', 'fstss.03@gmail.com');
+ 
+
+insert into events (events_name, events_latitude, events_longitude, events_idate, events_fdate) values('Iade Games',	38.707146, -9.152381, str_to_date('2023.11.21','%Y.%m.%d'), str_to_date('2023.11.30','%Y.%m.%d'));
+insert into events (events_name, events_latitude, events_longitude, events_idate, events_fdate) values('Web Summit Liboa', 38.768279, -9.094256, str_to_date('2024.04.10','%Y.%m.%d'), str_to_date('2024.04.15','%Y.%m.%d'));
+insert into events (events_name, events_latitude, events_longitude, events_idate, events_fdate) values('All Stadiums', null, null, str_to_date('2024.01.01','%Y.%m.%d'), null);
+insert into events (events_name, events_latitude, events_longitude, events_idate, events_fdate) values('Beach Collector',	null, null, str_to_date('2024.02.03','%Y.%m.%d'), null);
+
+
+insert into lqrcode (lqrcode_latitude, lqrcode_longitude, lqrcode_altitude) values (38.752986, -9.183192, 85);	  #Estádio do Benfica 	
+insert into lqrcode (lqrcode_latitude, lqrcode_longitude, lqrcode_altitude) values (38.762429, -9.160948, 92);    #Estádio do Sporting
+insert into lqrcode (lqrcode_latitude, lqrcode_longitude, lqrcode_altitude) values (38.708832, -9.259234, 35);    #Estádio do Jamor
+insert into lqrcode (lqrcode_latitude, lqrcode_longitude, lqrcode_altitude) values (38.707206, -9.152476, 5);     #IADE porta principal
+insert into lqrcode (lqrcode_latitude, lqrcode_longitude, lqrcode_altitude) values (38.680549, -9.33595, 9);      #Carcavelos praia   
+insert into lqrcode (lqrcode_latitude, lqrcode_longitude, lqrcode_altitude) values (38.7029, -9.398329,	5);       #Tamariz praia
+insert into lqrcode (lqrcode_latitude, lqrcode_longitude, lqrcode_altitude) values (38.730121, -9.473425, 10);    #Guincho praia
+insert into lqrcode (lqrcode_latitude, lqrcode_longitude, lqrcode_altitude) values (38.768584, -9.095272, 9);     #Web summit
+
+
+insert into post (post_foto, post_text) values (null, 'muito giro');
+insert into post (post_foto, post_text) values (null, 'melhor de portugal');
+insert into post (post_foto, post_text) values (null, 'adorei');
+insert into post (post_foto, post_text) values (null, 'boa praia');
+insert into post (post_foto, post_text) values (null, 'agua bem fria');
+insert into post (post_foto, post_text) values (null, 'vou voltar');
+insert into post (post_foto, post_text) values (null, 'spooorting');
+insert into post (post_foto, post_text) values (null, 'estadio feio');
+insert into post (post_foto, post_text) values (null, 'boas memorias');
+insert into post (post_foto, post_text) values (null, 'melhores ondas');
+insert into post (post_foto, post_text) values (null, 'muita gente');
+insert into post (post_foto, post_text) values (null, 'farto de estudar');
+insert into post (post_foto, post_text) values (null, 'mais um dia');
+insert into post (post_foto, post_text) values (null, 'dia de praia');
+insert into post (post_foto, post_text) values (null, 'visita ao jamor');
+insert into post (post_foto, post_text) values (null, 'date night');
+insert into post (post_foto, post_text) values (null, 'bom diaaa');
+insert into post (post_foto, post_text) values (null, 'olaaaaa');
+insert into post (post_foto, post_text) values (null, 'boa semana');
+insert into post (post_foto, post_text) values (null, 'dia de sol');
+insert into post (post_foto, post_text) values (null, 'dia de jogo');
+insert into post (post_foto, post_text) values (null, 'bifaninha');
+insert into post (post_foto, post_text) values (null, 'taça de portugal');
+insert into post (post_foto, post_text) values (null, 'solzinho');
+insert into post (post_foto, post_text) values (null, 'meu deus que gelo');
+insert into post (post_foto, post_text) values (null, 'bom tempo');
+insert into post (post_foto, post_text) values (null, 'que seca');
+insert into post (post_foto, post_text) values (null, 'buenos dias');
+insert into post (post_foto, post_text) values (null, 'mais um dia');
+insert into post (post_foto, post_text) values (null, 'que jogo');
+insert into post (post_foto, post_text) values (null, 'bom jogo');
+insert into post (post_foto, post_text) values (null, 'o grande');
+insert into post (post_foto, post_text) values (null, 'mais uma');
+
+
+insert into achivements (achivements_name) values ('Collect all stadiums');
+insert into achivements (achivements_name) values ('Go to all Beatches');
+insert into achivements (achivements_name) values ('Web Summit collector');
+insert into achivements (achivements_name) values ('Iade games collector');
+
+
+insert into ulq (ulq_user_id, ulq_post_id, ulq_lqrcode_id) values (1, 6, 8);       #Tiago Estrela     vou voltar	        #Web summit                 
+insert into ulq (ulq_user_id, ulq_post_id, ulq_lqrcode_id) values (1, 9, 4);       #Tiago Estrela     boas memorias	    #IADE porta principal         
+insert into ulq (ulq_user_id, ulq_post_id, ulq_lqrcode_id) values (1, 3, 1);       #Tiago Estrela     adorei	            #Estádio do Benfica  
+insert into ulq (ulq_user_id, ulq_post_id, ulq_lqrcode_id) values (2, 11, 5);       #Diogo Menezes     muita gente	        #Carcavelos praia     
+insert into ulq (ulq_user_id, ulq_post_id, ulq_lqrcode_id) values (2, 10, 6);       #Diogo Menezes     melhores ondas	    #Tamariz praia         
+insert into ulq (ulq_user_id, ulq_post_id, ulq_lqrcode_id) values (2, 4, 7);       #Diogo Menezes     boa praia	        #Guincho praia     
+insert into ulq (ulq_user_id, ulq_post_id, ulq_lqrcode_id) values (3, 22, 3);       #Diogo Couto       bifaninha	        #Estádio do Jamor     
+insert into ulq (ulq_user_id, ulq_post_id, ulq_lqrcode_id) values (3, 19, 5);       #Diogo Couto       boa semana	        #Carcavelos praia     
+insert into ulq (ulq_user_id, ulq_post_id, ulq_lqrcode_id) values (3, 24, 6);       #Diogo Couto       solzinho	            #Tamariz praia 
+insert into ulq (ulq_user_id, ulq_post_id, ulq_lqrcode_id) values (4, 1, 3);       #João Figueiras    muito giro           #Estádio do Jamor     
+insert into ulq (ulq_user_id, ulq_post_id, ulq_lqrcode_id) values (4, 26, 5);       #João Figueiras    bom tempo            #Carcavelos praia    
+insert into ulq (ulq_user_id, ulq_post_id, ulq_lqrcode_id) values (4, 27, 4);       #João Figueiras    que seca 	        #IADE porta principal     
+insert into ulq (ulq_user_id, ulq_post_id, ulq_lqrcode_id) values (5, 31, 1);       #André Guimarães   bom jogo  	        #Estádio do Benfica      
+insert into ulq (ulq_user_id, ulq_post_id, ulq_lqrcode_id) values (5, 5, 6);       #André Guimarães   agua bem fria        #Tamariz praia         
+insert into ulq (ulq_user_id, ulq_post_id, ulq_lqrcode_id) values (5, 28, 3);       #André Guimarães   buenos dias  	    #Estádio do Jamor         
+insert into ulq (ulq_user_id, ulq_post_id, ulq_lqrcode_id) values (6, 21, 2);       #António Costa     dia de jogo          #Estádio do Sporting     
+insert into ulq (ulq_user_id, ulq_post_id, ulq_lqrcode_id) values (6, 25, 6);       #António Costa     meu deus que gelo    #Tamariz praia         
+insert into ulq (ulq_user_id, ulq_post_id, ulq_lqrcode_id) values (6, 33, 3);       #António Costa     mais uma             #Estádio do Jamor
+insert into ulq (ulq_user_id, ulq_post_id, ulq_lqrcode_id) values (7, 18, 8);       #Gaspar Madeira    olaaaaa              #Web summit
+insert into ulq (ulq_user_id, ulq_post_id, ulq_lqrcode_id) values (7, 12, 4);       #Gaspar Madeira    farto de estudar     #IADE porta principal        
+insert into ulq (ulq_user_id, ulq_post_id, ulq_lqrcode_id) values (7, 32, 1);       #Gaspar Madeira    o grande             #Estádio do Benfica 
+insert into ulq (ulq_user_id, ulq_post_id, ulq_lqrcode_id) values (8, 17, 8);       #Duarte Alves      bom diaaa            #Web summit 
+insert into ulq (ulq_user_id, ulq_post_id, ulq_lqrcode_id) values (8, 16, 7);       #Duarte Alves      date_night           #Guincho praia  
+insert into ulq (ulq_user_id, ulq_post_id, ulq_lqrcode_id) values (8, 14, 6);       #Duarte Alves      dia de praia         #Tamariz praia    
+insert into ulq (ulq_user_id, ulq_post_id, ulq_lqrcode_id) values (9, 2, 5);       #Leandro Silva     melhor de portugal   #Carcavelos praia          
+insert into ulq (ulq_user_id, ulq_post_id, ulq_lqrcode_id) values (9, 20, 6);       #Leandro Silva     dia de sol           #Tamariz praia  
+insert into ulq (ulq_user_id, ulq_post_id, ulq_lqrcode_id) values (9, 13, 7);       #Leandro Silva     mais um dia          #Guincho praia   
+insert into ulq (ulq_user_id, ulq_post_id, ulq_lqrcode_id) values (10, 30, 1);       #Joana Sousa       que jogo             #Estádio do Benfica 
+insert into ulq (ulq_user_id, ulq_post_id, ulq_lqrcode_id) values (10, 29, 2);       #Joana Sousa       mais um dia          #Estádio do Sporting   
+insert into ulq (ulq_user_id, ulq_post_id, ulq_lqrcode_id) values (10, 15, 3);       #Joana Sousa       visita ao jamor      #Estádio do Jamor       
+insert into ulq (ulq_user_id, ulq_post_id, ulq_lqrcode_id) values (11, 7, 2);       #Filipe Santos     spooorting           #Estádio do Sporting  
+insert into ulq (ulq_user_id, ulq_post_id, ulq_lqrcode_id) values (11, 8, 1);       #Filipe Santos     estadio feio         #Estádio do Benfica     
+insert into ulq (ulq_user_id, ulq_post_id, ulq_lqrcode_id) values (11, 23, 3);       #Filipe Santos     taça de portugal     #Estádio do Jamor         
+
+insert into lqe (lqe_lqrcode_id, lqe_events_id) values (1, 3);      #Estádio do Benfica 	All Stadiums	
+insert into lqe (lqe_lqrcode_id, lqe_events_id) values (2, 3);      #Estádio do Sporting	All Stadiums
+insert into lqe (lqe_lqrcode_id, lqe_events_id) values (3, 3);      #Estádio do Jamor	    All Stadiums
+insert into lqe (lqe_lqrcode_id, lqe_events_id) values (4, 1);      #IADE porta principal	Iade Games
+insert into lqe (lqe_lqrcode_id, lqe_events_id) values (5, 4);      #Carcavelos praia	    Beach Collector
+insert into lqe (lqe_lqrcode_id, lqe_events_id) values (6, 4);      #Tamariz praia	        Beach Collector
+insert into lqe (lqe_lqrcode_id, lqe_events_id) values (7, 4);      #Guincho praia	        Beach Collector
+insert into lqe (lqe_lqrcode_id, lqe_events_id) values (8, 2);      #Web summit	Web         Summit Liboa
+
+
+insert into achlqe (achlqe_lqe_id, achlqe_achivements_id) values (1, 3);        #Collect all stadiums	All Stadiums
+insert into achlqe (achlqe_lqe_id, achlqe_achivements_id) values (2, 4);        #Go_to all Beatches	    Beach Collector
+insert into achlqe (achlqe_lqe_id, achlqe_achivements_id) values (3, 2);        #Web Summit collector	Web Summit Liboa
+insert into achlqe (achlqe_lqe_id, achlqe_achivements_id) values (4, 1);        #Iade games collector	Iade Games
+
+
+commit;
+          
