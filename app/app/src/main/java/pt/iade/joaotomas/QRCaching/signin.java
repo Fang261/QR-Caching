492643e96e84facdff794d7d0281bcafb4e12b34
PrimedFortuna <Primed.5353@gmail.com>
2024-01-07 08:23:47 +0000

    Non functional signin and login implemented XML still needs to be fixed

--- app/app/src/main/java/pt/iade/joaotomas/QRCaching/signin.java
+++ app/app/src/main/java/pt/iade/joaotomas/QRCaching/signin.java
@@ -10,39 +10,74 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
+import java.util.ArrayList;
+
+import pt.iade.joaotomas.QRCaching.models.UserItem;
+
 public class signin extends AppCompatActivity {
 
-private Button loginbutton;
-private EditText email;
-private Button confirmbutton;
+    private Button loginbutton;
+    private EditText usernameEditText;
+    private EditText idEditText;
+    private EditText passwordEditText;
+    private Button confirmbutton;
+
+    private ArrayList<UserItem> UserList;
+
+    private void handleSignInResult(int newCreatedID,String signedInUsername, String signedInPassword) {
+        Intent resultIntent = new Intent();
+        resultIntent.putExtra("username", signedInUsername);
+        resultIntent.putExtra("password", signedInPassword);
+        resultIntent.putExtra("ID", newCreatedID);
+        setResult(RESULT_OK, resultIntent);
+        finish();
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signin);
 
+        usernameEditText = findViewById(R.id.register_username_edit);
+        idEditText = findViewById(R.id.register_id_edit);
+        passwordEditText = findViewById(R.id.register_pw_edit);
+        loginbutton = findViewById(R.id.login_button2);
+        confirmbutton = findViewById(R.id.confirm_button);
 
-        loginbutton = (Button) (findViewById(R.id.login_button2));
-        confirmbutton = (Button) (findViewById(R.id.confirm_button));
+        UserList = new ArrayList<>();
+
         loginbutton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent=new Intent(signin.this,MainActivity.class);
+                Intent intent = new Intent(signin.this, MainActivity.class);
                 startActivity(intent);
             }
         });
+
         confirmbutton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-         //       Intent intent=new Intent(signin.this,mainpage.class);
-         //       startActivity(intent);
+                String enteredUsername = usernameEditText.getText().toString();
+                int enteredId = Integer.parseInt(idEditText.getText().toString());
+                String enteredPassword = passwordEditText.getText().toString();
+                int newId = UserItem.getLastAssignedId() + 1;
+                UserItem newUser = new UserItem(newId, enteredUsername, enteredPassword, null, null);
+                UserList.add(newUser);
+                UserItem.setLastAssignedId(newId);
+
                 AlertDialog.Builder builder = new AlertDialog.Builder(signin.this);
-                builder.setTitle("WIP");
-                builder.setPositiveButton("WIP", new DialogInterface.OnClickListener() {
+                builder.setTitle("Account Created");
+                builder.setMessage("Your account has been created successfully.");
+                builder.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialogInterface, int i) {
                         dialogInterface.dismiss();
+                        Intent intent = new Intent(signin.this, MainActivity.class);
+                        startActivity(intent);
                     }
-                }).show();
+                });
+                builder.show();
+                handleSignInResult(newId,newUser.getUsername(), newUser.getPassword());
             }
         });
     }
